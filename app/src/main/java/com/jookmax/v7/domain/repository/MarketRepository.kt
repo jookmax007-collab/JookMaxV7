69b1ac8a3e0b24203a7be3c69dce580b2994950b
Saeed <jookmax007@gmail.com>
2026-09-06 04:31:49 -0700

    Add market repository contract

---
+++ app/src/main/java/com/jookmax/v7/domain/repository/MarketRepository.kt
@@ -0,0 +1,19 @@
+package com.jookmax.v7.domain.repository
+
+import com.jookmax.v7.core.model.MarketPrice
+
+
+interface MarketRepository {
+
+
+    suspend fun getLatestMarketPrice(): MarketPrice?
+
+
+
+    fun getCachedMarketPrice(): MarketPrice?
+
+
+
+    fun clearCache()
+
+}
