106d355a466d71f887785585b881e5b221427e8e
Saeed <jookmax007@gmail.com>
2026-09-06 04:33:59 -0700

    Add get market price use case

---
+++ app/src/main/java/com/jookmax/v7/domain/usecase/GetMarketPriceUseCase.kt
@@ -0,0 +1,22 @@
+package com.jookmax.v7.domain.usecase
+
+import com.jookmax.v7.core.model.MarketPrice
+import com.jookmax.v7.domain.repository.MarketRepository
+
+
+class GetMarketPriceUseCase(
+
+    private val repository: MarketRepository
+
+) {
+
+
+    suspend operator fun invoke(): MarketPrice? {
+
+
+        return repository.getLatestMarketPrice()
+
+    }
+
+
+}
