34c88ff8699859cec7976dd913e5c9595127b776
Saeed <jookmax007@gmail.com>
2026-09-07 10:41:24 -0700

    feat: extend market api service endpoints

--- app/src/main/java/com/jookmax/v7/data/remote/MarketApiService.kt
+++ app/src/main/java/com/jookmax/v7/data/remote/MarketApiService.kt
@@ -1,7 +1,11 @@
 package com.jookmax.v7.data.remote
 
 
+import com.jookmax.v7.data.remote.dto.CandleDto
+import com.jookmax.v7.data.remote.dto.MarketQuoteDto
+import com.jookmax.v7.data.remote.dto.TickDto
 import com.jookmax.v7.data.remote.dto.MarketPriceDto
+
 import retrofit2.http.GET
 
 
@@ -9,9 +13,23 @@ import retrofit2.http.GET
 interface MarketApiService {
 
 
-
     @GET("market/latest")
     suspend fun getLatestMarketPrice(): MarketPriceDto?
+
+
+
+    @GET("market/quote")
+    suspend fun getMarketQuote(): MarketQuoteDto?
+
+
+
+    @GET("market/tick")
+    suspend fun getLatestTick(): TickDto?
+
+
+
+    @GET("market/candles")
+    suspend fun getCandles(): List<CandleDto>
 
 
 
