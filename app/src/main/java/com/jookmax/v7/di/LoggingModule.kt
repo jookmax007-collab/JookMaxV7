23c865871eecf627583f9381db75fbdd2a8cd9ef
Saeed <jookmax007@gmail.com>
2026-09-07 08:30:07 -0700

    di: bind logging repository layer

--- app/src/main/java/com/jookmax/v7/di/LoggingModule.kt
+++ app/src/main/java/com/jookmax/v7/di/LoggingModule.kt
@@ -4,7 +4,11 @@ package com.jookmax.v7.di
 import com.jookmax.v7.core.logging.JookMaxLogger
 import com.jookmax.v7.core.logging.LogStorage
 import com.jookmax.v7.core.logging.Logger
+
 import com.jookmax.v7.data.logging.InMemoryLogStorage
+import com.jookmax.v7.data.logging.LogRepositoryImpl
+
+import com.jookmax.v7.domain.logging.LogRepository
 
 import dagger.Binds
 import dagger.Module
@@ -15,6 +19,19 @@ import javax.inject.Singleton
 
 
 
+/**
+ * Provides logging dependencies.
+ *
+ * Architecture:
+ *
+ * Logger
+ *   ↓
+ * LogRepository
+ *   ↓
+ * LogStorage
+ *   ↓
+ * InMemoryLogStorage
+ */
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class LoggingModule {
@@ -29,11 +46,24 @@ abstract class LoggingModule {
 
 
 
+
+
+    @Binds
+    @Singleton
+    abstract fun bindLogRepository(
+        repository: LogRepositoryImpl
+    ): LogRepository
+
+
+
+
+
     @Binds
     @Singleton
     abstract fun bindLogStorage(
         storage: InMemoryLogStorage
     ): LogStorage
+
 
 
 }
